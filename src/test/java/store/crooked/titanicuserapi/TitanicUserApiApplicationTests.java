c8e55088b300f9791deb932303b155eb206c85d2
jungssg <younwjdtjr@naver.com>
2023-08-07 10:33:24 +0900

    start

---
+++ src/test/java/store/crooked/titanicuserapi/TitanicUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package store.crooked.titanicuserapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitanicUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
